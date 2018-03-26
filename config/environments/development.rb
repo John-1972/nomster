56cc0ec8b2d05c8bdaee2a1fe4e2180b9d95aa7d
John Henderson <johnhenderson@fastmail.fm>
2018-03-26 18:55:13 +0000

    Code added to send mails from GMail account

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,20 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
+# The following code is to send e-mails from a GMail account... (Addr and p/w in application.yml)
   
+# config.action_mailer.delivery_method = :smtp
+
+#  config.action_mailer.smtp_settings = {
+#    address: "smtp.gmail.com",
+#    port: 587,
+#    domain: "example.com",
+#    authentication: "plain",
+#    enable_starttls_auto: true,
+#    user_name: ENV['GMAIL_ADDRESS'],
+#    password: ENV['GMAIL_PASSWORD']
+#  }
+
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   
   # In the development environment your application's code is reloaded on
