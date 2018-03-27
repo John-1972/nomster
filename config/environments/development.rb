bad4d1ed6eaa41af1033f38902ca81fed5ee0d96
John Henderson <johnhenderson@fastmail.fm>
2018-03-27 12:11:12 +0000

    Adjustment of automated e-mail settings & content

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,17 +3,17 @@ Rails.application.configure do
 
 # The following code is to send e-mails from a GMail account... (Addr and p/w in application.yml)
   
-# config.action_mailer.delivery_method = :smtp
+ config.action_mailer.delivery_method = :smtp
 
-#  config.action_mailer.smtp_settings = {
-#    address: "smtp.gmail.com",
-#    port: 587,
-#    domain: "example.com",
-#    authentication: "plain",
-#    enable_starttls_auto: true,
-#    user_name: ENV['GMAIL_ADDRESS'],
-#    password: ENV['GMAIL_PASSWORD']
-#  }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mail.yahoo.com",
+    port: 465,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['YAHOO_ADDRESS'],
+    password: ENV['YAHOO_PASSWORD']
+  }
 
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   
