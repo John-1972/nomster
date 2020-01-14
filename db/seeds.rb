7d4a27b9579a488563a8804234695a2ed7e85cc2
John Henderson <johnhenderson@fastmail.fm>
2020-01-14 18:02:36 +0000

    Created a seed file with 14 database records

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(name: 'Area 4', address: '112 Main Street, Cambridge, MA 02139', description: 'One of the best Pizza places. Their Mushroom pizza is unsurpassed!')
+Place.create(name: 'Artie\'s Kitchen', address: '33 Southgate, Chichester, West Sussex', description: 'A cozy restaurant with a great selection of Spanish tapas.')
+Place.create(name: 'ASK Italian', address: '38 East Street, Chichester, West Sussex', description: 'A classy restaurant in which to enjoy Italian cuisine.')
+Place.create(name: 'Bill\'s', address: '3 Butter Market, North Street, Chichester', description: 'A spacious bistro with a great atmosphere serving breakfast, lunch or dinner.')
+Place.create(name: 'Carluccio\'s', address: '2 Eastgate Square, Chichester, West Sussex', description: 'A popular restaurant serving light Italian meals from 8am until 11pm.')
+Place.create(name: 'Confucius', address: '2 Cooper Street, Chichester, West Sussex', description: 'A friendly restaurant in the centre of town offering tasty Chinese cuisine.')
+Place.create(name: 'Cote Brasserie', address: '63 South Street, Chichester, West Sussex', description: 'A restaurant serving French food for breakfast, lunch & dinner.')
+Place.create(name: 'Pizza Express', address: '27 South Street, Chichester, West Sussex', description: 'Lovely crispy crust pizzas in a great atmosphere.')
+Place.create(name: 'Prezzo', address: '61 South Street, Chichester, West Sussex', description: 'A family-friendly restaurant serving Italian food.')
+Place.create(name: 'Real Burger Kitchen', address: '5-6 South Street, Chichester, West Sussex', description: 'A great choice of burgers, served with fries & cold drinks.')
+Place.create(name: 'Spire\'s Bakery', address: '3A Crane Street, Chichester, West Sussex', description: 'A small & friendly cafe that offers good English food.')
+Place.create(name: 'The Chantry', address: '27-28 Southgate, Chichester, West Sussex', description: 'A great place to order stone-baked pizza.')
+Place.create(name: 'Wildwood', address: '30 Southgate, Chichester, West Sussex', description: 'A traditional restaurant which specialises in pizza & pasta.')
+Place.create(name: 'Wagamama', address: '26 South Street, Chichester, West Sussex', description: 'A very popular restaurant serving Japanese and Asian cuisine.')
